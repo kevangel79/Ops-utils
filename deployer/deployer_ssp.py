f9ef31145caf15d264df34be55fa80bbbb93610b
kevangel79 <58726215+kevangel79@users.noreply.github.com>
2020-09-21 11:23:11 +0300

    Update deployer_ssp.py

--- deployer/deployer_ssp.py
+++ deployer/deployer_ssp.py
@@ -2,6 +2,7 @@
 
 import json, sched, time
 import subprocess
+import os
 import argparse
 from ServiceRegistryAms import PullPublish
 
@@ -10,20 +11,37 @@ def init_ams_agent():
         config = json.load(json_data_file)
     return PullPublish(config)
 
+'''
+This function will return the altered current state and new additions to it
+    update_data gets 2 arguments:
+    
+    - registries, which represents the current state in json
+    - messages, the new incoming messages in json
+'''
 def update_data( registries, messages ):
     new_registries = []
     for msg in messages:
-        is_reconfiguration = False
+        new_entry = True
         for registry in registries:
             if registry['registry_service_id'] == msg['id']:
-                is_reconfiguration = True                
-                registry['whitelist'] = [ msg['entity_id'] ]
-                registry['src'] = msg['metadata_url']
+                new_entry = False
+                if msg['deleted'] == False:      
+                    registry['whitelist'] = [ msg['entity_id'] ]
+                    registry['src'] = msg['metadata_url']
+                else:
+                    registries.remove(registry)
                 break
-        if is_reconfiguration == False:
+        if new_entry == True:
             new_registries.append({'registry_service_id': msg['id'], 'whitelist': [msg['entity_id']], 'src': msg['metadata_url']})
     return registries + new_registries
 
+'''
+This function get the current state and will generate a configuration
+php file with the updated state
+    generate_config gets 2 arguments:
+    - registries, which represents the current state in json
+    - path, which is the location of the config file
+'''
 def generate_config( registries, path ):
     header = """<?php
 return array("""
@@ -45,15 +63,17 @@ return array("""
     
 
 if __name__ == '__main__':
+    # Get config path from arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', required=True, type=str, help="Provide the path of the ssp metadata configuration file")
     args = parser.parse_args()
     path = args.c
+
     ams = init_ams_agent()
+    # Get current registries
     registries_data = subprocess.run(['php','./utils/json_export.php', path], universal_newlines=True, stdout=subprocess.PIPE)
     registries_json = json.loads(registries_data.stdout)
-    #messages = ams.pull(1) # Get messages
-    messages = [{'id': 666, 'service_name': 'Saml reconfigure name', 'service_description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in ex in tellus congue commodo. Suspendisse condimentum purus ante, in ornare leo egestas ut.', 'logo_uri': 'https://cdn.auth0.com/blog/duo-saml-exploit/saml.png', 'policy_uri': 'https://policy_uri.com', 'integration_environment': 'demo', 'protocol': 'saml', 'metadata_url': 'https://metadataurl.com', 'entity_id': 'https://saml-id-1_reconfigure.com', 'deleted': False, 'contacts': [{'email': 'mygrail@gmail.com', 'type': 'admin'}, {'email': 'myfail@gmail.com', 'type': 'admin'}]}, {'id': 8, 'service_name': ' Registration Service', 'service_description': 'https://dcassetcdn.com/design_img/2772494/655674/655674_15180795_2772494_df327367_image.jpg', 'logo_uri': 'https://dcassetcdn.com/design_img/2772494/655674/655674_15180795_2772494_df327367_image.jpg', 'policy_uri': 'https://dcassetcdn.com/design_img/2772494/655674/655674_15180795_2772494_df327367_image.jpg', 'integration_environment': 'demo', 'protocol': 'saml', 'metadata_url': 'https://metadata.com', 'entity_id': 'https://entity_id.com', 'deleted': False, 'contacts': [{'email': 'asdasdasda@asdfsadf.com', 'type': 'admin'}]}]
+    messages = ams.pull(1) # Get messages
     messages_counter = len(messages)
     while len(messages)>0:
         registries_json = update_data(registries_json, messages)
@@ -61,4 +81,5 @@ if __name__ == '__main__':
         messages_counter += len(messages)
     if messages_counter > 0:
         generate_config(registries_json, path)
-        #subprocess.run(['curl','http://localhost/proxy/module.php/cron/cron.php?key=TOPSECRET&tag=hourly'])
+        secret_key=os.environ['SECRET_KEY']
+        subprocess.run(['echo','http://localhost/proxy/module.php/cron/cron.php?key='+secret_key+'&tag=hourly'])
