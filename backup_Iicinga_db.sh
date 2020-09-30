a455cd5b1c64e6982b5535db6ba7f20941cdf87d
kevangel79 <58726215+kevangel79@users.noreply.github.com>
2020-09-30 15:02:19 +0300

    Create backup_Iicinga_db.sh

---
+++ backup_Iicinga_db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+DATE=`date +%d-%m-%y`
+
+# Create directory to copy files for back up
+TEMP_DIR="/var/backup_db/backup_icinga_db_${DATE}/"
+mkdir -p ${TEMP_DIR}
+cd /home/postgres
+sudo -u postgres pg_dumpall > /home/postgres/backup_icinga_db_${DATE}.bak
+
+# Copy NextCloud files to backup directory
+cp /home/postgres/backup_icinga_db_${DATE}.bak ${TEMP_DIR}
+
+# Create a tarball with the backup
+cd /var/backup_db/
+tar -czvf backup_icinga_db_${DATE}.tar.gz ./backup_icinga_db_${DATE}
+
+# Remove temp dir
+rm -rf ${TEMP_DIR}
+rm /home/postgres/backup_icinga_db_${DATE}.bak
+
+# Copy files to Archive server (Read script bellow for more information)
+/root/utils/rSyncBackups.sh -u root -r snf-11813.ok-kno.grnetcloud.net -k ~/.ssh/id_rsa -f /var/backup_db/backup_icinga_db_${DATE}.tar.gz -t /home/backups/icinga
